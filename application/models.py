de12768e45821418e1ca9a946be09096a53e32bd
anas aboungab <aaboungab@qa.com>
2020-11-10 19:46:19 +0000

    In progress

--- application/models.py
+++ application/models.py
@@ -1,14 +1,14 @@
 from application import db 
 
 class Series(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(30), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String(30), nullable=False)
-    seasons = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    category = db.Column(db.String(30))
+    seasons = db.Column(db.Integer)
+    revs = db.relationship('Review', backref='series')
 
 class Review(db.Model):
-    reviewid = db.Column(db.Integer, primary_key=True, nullable=False)
-    descrp = db.Column(db.String(300), nullable=False)
-    rating = db.Column(db.Integer(5), nullable=False)
-    seriesid = db.Column(db.Integer, db.ForeignKey('Series.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    desc = db.Column(db.String(30), unique=True )
+    rating = db.Column(db.Integer)
+    seriesid = db.Column(db.Integer, db.ForeignKey('series.id'))
