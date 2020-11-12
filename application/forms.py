dec41ae092efff8e13e70743db081bb65aa44915
anas aboungab <aaboungab@qa.com>
2020-11-12 16:36:43 +0000

    In progress

--- application/forms.py
+++ application/forms.py
@@ -27,5 +27,5 @@ class ReviewForm(FlaskForm):
 			DataRequired()])
     rating = SelectField('Ratings',
 		choices=[
-			('5/5','5'),('4/5','4'),('3/5','3'),('2/5','2'),('1/5','1'), ('0/5','0')])
+			('5', '5/5'),('4','4/5'),('3','3/5'),('2','2/5'),('1','1/5'), ('0','0/5')])
     submit = SubmitField('Add Review')
